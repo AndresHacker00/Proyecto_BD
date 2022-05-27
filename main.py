687a2e5cf1fc3ceec1b334ff4fc89897bc0aee41
AndresHacker00 <69330409+AndresHacker00@users.noreply.github.com>
2022-05-27 11:24:21 -0700

    https://github.com/AndresHacker00/Proyecto_BD.git

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import*
+from ventana_crud import*
+
+def main():
+    root = Tk()
+    root.wm_title("Aduana BD")
+    app = Ventana(root)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()     
